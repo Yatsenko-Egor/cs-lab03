9baaa53e3b975d45e5a542bfa13f49e9c9498d92
YatsenkoEV <gretchin777@gmail.com>
2023-07-04 16:23:55 +0300

    code: добавлен вывод в формате svg

--- p3/p3.cpp
+++ p3/p3.cpp
@@ -1,7 +1,6 @@
 ﻿#include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -13,6 +12,11 @@ size_t find_max_count(const vector<size_t>& bins, const size_t bin_count);
 void show_histogram_text(const vector<size_t>& bins, const size_t bin_count);
 void output_numbers(const vector<double>& numbers, const size_t& count);
 
+void svg_begin(double width, double height);
+void svg_end();
+void show_histogram_svg(const vector<size_t>& bins);
+void svg_text(double left, double baseline, string text);
+void svg_rect(double x, double y, double width, double height, string stroke = "black", string fill = "black");
 
 int main()
 {
@@ -26,7 +30,48 @@ int main()
     cerr << "Enter bin count: ";
     cin >> bin_count;
     vector<size_t> bins = make_histogram(numbers, bin_count);
-    show_histogram_text(bins, bin_count);
+    show_histogram_svg(bins);
+}
+
+void svg_begin(double width, double height) {
+    cout << "<?xml version='1.0' encoding='UTF-8'?>\n";
+    cout << "<svg ";
+    cout << "width='" << width << "' ";
+    cout << "height='" << height << "' ";
+    cout << "viewBox='0 0 " << width << " " << height << "' ";
+    cout << "xmlns='http://www.w3.org/2000/svg'>\n";
+}
+
+void svg_end() {
+    cout << "</svg>\n";
+}
+
+void show_histogram_svg(const vector<size_t>& bins) {
+    const auto IMAGE_WIDTH = 400;
+    const auto IMAGE_HEIGHT = 300;
+    const auto TEXT_LEFT = 20;
+    const auto TEXT_BASELINE = 20;
+    const auto TEXT_WIDTH = 50;
+    const auto BIN_HEIGHT = 30;
+    const auto BLOCK_WIDTH = 10;
+
+    svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    double top = 0;
+    for (size_t bin : bins) {
+        const double bin_width = BLOCK_WIDTH * bin;
+        svg_text(TEXT_LEFT, top + TEXT_BASELINE, to_string(bin));
+        svg_rect(TEXT_WIDTH, top, bin_width, BIN_HEIGHT, "red", "#ffeeee");
+        top += BIN_HEIGHT;
+    }
+    svg_end();
+}
+void svg_text(double left, double baseline, string text) {
+    cout << "<text x='" << left << "' y='" << baseline << "'>" << text << "</text>" << '\n';
+}
+
+void svg_rect(double x, double y, double width, double height,string stroke, string fill) {
+    cout << "<rect x=' " << x << "' y=' " << y << "' width=' " << width << "' height=' " << height << "' stroke=' " << stroke << "' fill='" << fill << "'/>" << '\n';
 }
 
 vector<double> input_numbers(size_t count) {
@@ -39,9 +84,9 @@ vector<double> input_numbers(size_t count) {
 
 void output_numbers(const vector<double> &numbers, const size_t &count) {
     for (size_t i = 0; i < count; i++) {
-        cout << numbers[i] << ' ';
+        cerr << numbers[i] << ' ';
     }
-    cout << '\n' << '\n';
+    cerr << '\n' << '\n';
 }
 
 void find_minmax(const vector<double>& numbers, double& min, double& max) {
