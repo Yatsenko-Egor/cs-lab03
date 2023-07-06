be36b66d03fd53030d546cea474934d197109cda
YatsenkoEV <gretchin777@gmail.com>
2023-07-06 17:08:56 +0300

    code: добавлен модуль функции find_minmax

--- p3/p3.cpp
+++ p3/p3.cpp
@@ -1,12 +1,12 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
+#include "histogram.h"
 
 using namespace std;
 
 void removeDuplicates(vector<double>& numbers, size_t& count);
 vector<double> input_numbers(size_t count);
-void find_minmax(const vector<double>& numbers, double& min, double& max);
 vector<size_t> make_histogram(const vector<double>& numbers, const size_t bin_count);
 size_t find_max_count(const vector<size_t>& bins, const size_t bin_count);
 void show_histogram_text(const vector<size_t>& bins, const size_t bin_count);
@@ -117,22 +117,9 @@ void output_numbers(const vector<double> &numbers, const size_t &count) {
     cerr << '\n' << '\n';
 }
 
-void find_minmax(const vector<double>& numbers, double& min, double& max) {
-    min = numbers[0];
-    max = numbers[0];
-    for (double x : numbers) {
-        if (x < min) {
-            min = x;
-        }
-        else if (x > max) {
-            max = x;
-        }
-    }
-}
-
 vector<size_t> make_histogram(const vector<double>& numbers, const size_t bin_count) {
     vector<size_t> bins(bin_count);
-    double min, max;
+    double min = 0, max = 0;
     find_minmax(numbers, min, max);
     double bin_size = (max - min) / bin_count;
     for (double number : numbers) {
