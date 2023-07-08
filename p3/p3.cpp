45e1ca1c1ce0f8fba24e7819dc9aefa88f76abd0
YatsenkoEV <gretchin777@gmail.com>
2023-07-08 12:51:22 +0300

    code: добавлена обработка ошибок

--- p3/p3.cpp
+++ p3/p3.cpp
@@ -12,10 +12,15 @@ using namespace std;
 int main(int argc,char* argv[]) {
     if (argc > 1) {
         CURL* curl = curl_easy_init();
+        //curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
         if (curl) {
             CURLcode res;
             curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
             res = curl_easy_perform(curl);
+            if (res != 0) {
+                cout << curl_easy_strerror(res);
+                exit(1);
+            }
         }
         curl_easy_cleanup(curl);
         return 0;
