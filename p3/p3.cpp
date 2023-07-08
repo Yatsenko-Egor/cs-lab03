acfa39181795d02611ae747bdad88b98ac5c79aa
YatsenkoEV <gretchin777@gmail.com>
2023-07-08 10:35:44 +0300

    code: добавлена библиотека CURL

--- p3/p3.cpp
+++ p3/p3.cpp
@@ -1,13 +1,17 @@
-﻿#include <iostream>
+﻿#pragma comment(lib, "libcurl.dll.a")
+
+#include <iostream>
 #include <vector>
 #include <string>
 #include "show_svg_histogram.h"
 #include "build_histogram.h"
+#include <curl\curl.h>
 
 using namespace std;
 
 int main()
 {
+    curl_global_init(CURL_GLOBAL_ALL);
     Input data = read_input(cin, false);
     remove_duplicates(data.numbers);
     vector<string> colors = input_colors(data.bin_count);
